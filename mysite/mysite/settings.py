f3d66ffbff40b94d562343bed659f1f27d98d84a
Azurhys <123641752+Azurhys@users.noreply.github.com>
2023-05-31 15:03:06 +0200

    FR NIGA

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a0^gra8oo=tnlandegr1%os*u^2egtc-+f_q^p03=9**so@%&j'
+SECRET_KEY = 'django-insecure-gvm92l!#ojmoul3gghnu%07w(tcxi-@n*%2%ihe(v8db4d+0x^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
